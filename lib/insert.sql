62f9b44a75a193541a435481bec15c19668e817e
Valencia Caldwell <VKCaldwell@gmail.com>
2021-12-23 22:58:06 -0600

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive, id) VALUES ('Mr. Chocolate', 3, 'M','brown', 'crazy', '320','0001'); 
+INSERT INTO bears (name, age, gender, color, temperament, alive, id) VALUES ('Rowdy', 17, 'M','brown', 'calm', '200','0002'); 
+INSERT INTO bears (name, age, gender, color, temperament, alive, id) VALUES ('Tabitha', 14, 'F','red', 'resilent', '560','0003'); 
+INSERT INTO bears (name, age, gender, color, temperament, alive, id) VALUES ('Sergeant Brown', 5, 'M','brown', 'crazy', '320','0004'); 
+INSERT INTO bears (name, age, gender, color, temperament, alive, id) VALUES ('Melissa', 1, 'M','black', 'crazy', '320','0005'); 
+INSERT INTO bears (name, age, gender, color, temperament, alive, id) VALUES ('Grinch', 7, 'M','brown', 'calm', '200','0006'); 
+INSERT INTO bears (name, age, gender, color, temperament, alive, id) VALUES ('Wendy', 4, 'F','red', 'resilent', '560','0007'); 
+INSERT INTO bears (name, age, gender, color, temperament, alive, id) VALUES (NULL, 24, 'F','grey', 'crazy', '320','0008'); 
